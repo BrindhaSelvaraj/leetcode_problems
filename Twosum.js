f02eaf7c3b3f7f0f18be53ed0c136877b0634a59
BrindhaSelvaraj <154896226+BrindhaSelvaraj@users.noreply.github.com>
2024-12-16 13:18:59 +0530

    Create Twosum.js

---
+++ Twosum.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums, target) {
+    const arr = [];
+    for(let i=0;i<nums.length;i++){
+        if((nums[i]+nums[i+1])==target){
+            arr.push(i,i+1);
+        }
+    }
+    return arr;
+};
